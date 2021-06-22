db3a6f0f8d73dd59608adbd81a78184cb072e212
w.f.sousa <wllyvn@gmail.com>
2021-06-22 09:47:47 -0300

    segundo tesde em ola

--- ola.py
+++ ola.py
@@ -1,2 +1,15 @@
 print('ola mundo')
 #teste no git e github#
+'git init  -->   para iniciar um repositorio'
+
+'git add  --> arquivo  para adicionar um arquivo'
+
+'git status  --> para ver se o arquivo ta ok'
+
+'git commit -m "descrição"  -->  lança o arquivo para "branch master"'
+
+'git branch main  -->  volta para "main"'
+
+'git remote add origin https://github.com/wll8090/<repositorio>'
+
+'git push -u origin main  -->  lança para o repositorio'
